6370e6361f48fcdacb29580b5b4f39ad99ac33b4
Tommi-Laptop <tommihiisi@gmail.com>
2023-02-04 01:06:13 +0200

    add new nutrient types

--- Assets/NutrientSpawner.cs
+++ Assets/NutrientSpawner.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class NutrientSpawner : MonoBehaviour
 {
-    public GameObject NutrientPrefab;
+    public GameObject BlueNutrientPrefab;
+    public GameObject RedNutrientPrefab;
+    public GameObject YellowNutrientPrefab;
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0;i < 10;i++)
         {
-            Instantiate(NutrientPrefab, new Vector3(Random.Range(-10,10), -Random.Range(1,10), 0), Quaternion.identity);
+            Instantiate(BlueNutrientPrefab, new Vector3(Random.Range(-10,10), -Random.Range(1,10), 0), Quaternion.identity);
         }
     }
 
