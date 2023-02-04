b6eded19393b387efa19e3ac5209cd75184a20c1
Tommi-Laptop <tommihiisi@gmail.com>
2023-02-04 04:27:17 +0200

    add UI Canvas add minimap add health bar add nutrient counter

--- Assets/Resources.cs
+++ Assets/Resources.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Resources : MonoBehaviour
 {
     public float Sunlight;
     public float Energy;
     public int Nutrients;
+    public Slider HealthSlider;
+    public TMP_Text NutrientText;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,18 @@ public class Resources : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Sunlight = Random.Range(0f, 1f);
+        if(Energy > Sunlight)
+        {
+            Energy -= 0.01f;
+        }
+        if(Energy < Sunlight)
+        {
+            Energy = Sunlight;
+        }
+        HealthSlider.value = Energy;
+        Nutrients++;
+        NutrientText.text = Nutrients.ToString();
     }
 
     public void Reset()
