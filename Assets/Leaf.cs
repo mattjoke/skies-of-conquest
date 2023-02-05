0613eecbec3c678483f5c181582c3d9b23e534b2
Tommi-Laptop <tommihiisi@gmail.com>
2023-02-05 09:28:44 +0200

    add thin stem sprite add properties for leaves

--- Assets/Leaf.cs
+++ Assets/Leaf.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Leaf : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float LeftEdge;
+    public float RightEdge;
+    public float Height;
+    public GameObject Shade;
 }
